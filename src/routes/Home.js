b83e396d480995a2359f24f9474afad30a3cb2aa
mintchocoicecream <sxxhnjng@gmail.com>
2022-02-08 20:26:09 +0900

    init commit

---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../css/Home.module.css";
+
+function Home() {
+  return (
+    <div className={styles.container}>
+        <h2>Home</h2>
+    </div>
+      );
+}
+
+export default Home;
