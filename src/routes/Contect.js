d1c50c0435895966eead30b7c129cc4524467712
mintchocoicecream <sxxhnjng@gmail.com>
2022-03-03 01:23:58 +0900

    Contact CSS

--- src/routes/Contect.js
+++ src/routes/Contect.js
@@ -22,9 +22,22 @@ function Contact() {
       </div>
       <div id={styles.info}>
         <div className={styles.content}>
-          <h3>페이지 작업 중...</h3>
+          <form className={styles.contactForm}>
+            <div className={styles.email}>
+              <label>보내는 사람 이메일</label>
+              <input type="email" placeholder="이메일주소@email.com" required/>
+            </div>
+            <div className={styles.msg}>
+              <label>제목</label>
+              <input type="text" placeholder="제목" required></input>
+              <label>내용</label>
+              <textarea placeholder="내용" rows="10" required></textarea>
+            </div>
+            <button type="submit">보내기</button>
+          </form>
         </div>
-      </div>    
+      </div>  
+      <h3>페이지 작업 중...</h3>  
     </div>
       );
 }
