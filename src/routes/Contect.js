8a10485c918eb4c0af9661ec97b1f60c9221d0af
mintchocoicecream <sxxhnjng@gmail.com>
2022-05-26 16:36:20 +0900

    Update Contact
    install EmailJS, Update Contact Form

--- src/routes/Contect.js
+++ src/routes/Contect.js
@@ -1,4 +1,5 @@
 import React from "react";
+import emailjs from '@emailjs/browser';
 import styles from "../css/About.module.css";
 
 function Contact() {
@@ -14,34 +15,47 @@ function Contact() {
     navCont.style.color = "#20c997";
     navHome.style.color = "#5a5a5a";
   }, []);
-  const onSubmit = (event) => {
-    event.preventDefault();
-  }
+
+  const inputs = document.getElementsByTagName("input");
+  const messages = document.getElementById("content");
+  const inputLi = []
+  inputLi.push(inputs[1], inputs[2], inputs[3], messages);
+
+  const SendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs.sendForm('service_pz6ou8j', 'template_rg2uug3', e.target, 'rUsmjHcmBH5XqZ_--')
+      .then((result) => {
+        // console.log(result.text);
+        inputLi.forEach(letter => letter.value = "");
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
 
   return (
     <main className={styles.container}>
             <div id={styles.title}>
         <h2>Contact</h2>
-      </div>
-      <h3>페이지 작업 중...</h3>  
+      </div> 
       <div id={styles.info}>
         <div className={styles.content}>
-          <form className={styles.contactForm}>
+          <form className={styles.contactForm} onSubmit={SendEmail}>
             <div className={styles.email}>
               <label>받는 사람 이메일 (To)</label>
               <input type="email" value="myminticecreamisthebest@gmail.com" disabled/>
               <label for="email">보내는 사람 이메일 (From) </label>
-              <input id="email" type="email" placeholder="example@email.com" required/>
+              <input id="email" type="email" name="email" placeholder="example@email.com" required/>
             </div>
             <div className={styles.msg}>
+              <label for="name">보내는 분 성함 (Name)</label>
+              <input id="name" type="text" name="name" placeholder="my name is..." />
               <label for="title">제목 (Title)</label>
-              <input id="title" type="text" placeholder="Hello:)" required></input>
-              <label>파일 첨부 (Attatchment)</label>
-              <input type="file" />
+              <input id="title" type="text" name="title" placeholder="Hello:)" required></input>
               <label for="content">내용 (Write)</label>
-              <textarea id="content" placeholder="내용(1,000자 제한)" rows="10" minLength="10" maxLength="1000" required></textarea>
+              <textarea id="content" name="message" placeholder="내용(1,000자 제한)" rows="10" minLength="10" maxLength="1000" required></textarea>
             </div>
-            <input id={styles.submit} onSubmit={onSubmit} type="submit" value="보내기"/>
+            <input id={styles.submit} type="submit" value="보내기"/>
           </form>
         </div>
       </div>  
