2177bc0e91a6ec20c7a3311b2fc6ae6fbf28e226
mintchocoicecream <sxxhnjng@gmail.com>
2022-06-13 21:30:00 +0900

    update Start

--- src/routes/Start.js
+++ src/routes/Start.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Start() {
-    const window_height = document.documentElement.scrollHeight;;
-    const icon_height = window.outerHeight;
+    // const window_height = document.documentElement.scrollHeight;;
+    // const icon_height = window.outerHeight;
 
   return (
-    <div className="start" style={{height:window_height}}>
-        <div className="start_content" style={{height: icon_height}}>
+    <div className="start">
+        <div className="start_content">
             <div className="icon"></div>
         </div>
     </div>
