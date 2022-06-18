60d9417c4cfda38e526db106fc3509d0a3f193e8
mintchocoicecream <sxxhnjng@gmail.com>
2022-06-18 17:31:26 +0900

    Update App.js
    Remove Start

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ function App() {
   <Router>
       <Switch>
         <Route exact path="/">
-          <Start />
+          {/* <Start /> */}
           <Navigation />
           <Header />
           <Home />
