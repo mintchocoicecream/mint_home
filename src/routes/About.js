5f3638083f9f53c66a508f06be55ed3cb981352e
mintchocoicecream <sxxhnjng@gmail.com>
2022-09-21 22:38:23 +0900

    Change Profile CSS

--- src/routes/About.js
+++ src/routes/About.js
@@ -20,8 +20,9 @@ function Profile() {
         </div>
         <div className="profileMyImg"></div>
         <div className="profileWrites">
-          <p>나는 그 텅 빈 방을 우리가 언제나 계획하고 꿈꿔왔던 실험실과 비교하지 않고, 그 자체로 받아들이고, 열심히 노력하면 얼마든지 바꿀 수 있는 가능성을 본 빌의 눈에 감탄했다. 과거의 꿈과 현재의 현실 사이에 커다란 격차가 있었지만 그는 우리의 새 삶을 사랑할 준비가 되어 있었다. 나도 그 삶을 사랑하기 위해 노력해보겠다고 결심했다.</p>
-          <div><p>- Lab Girl 中, Hope Jahren</p></div>
+          <p>나는 그 텅 빈 방을 우리가 언제나 계획하고 꿈꿔왔던 실험실과 비교하지 않고, 그 자체로 받아들이고, 열심히 노력하면 얼마든지 바꿀 수 있는 가능성을 본 빌의 눈에 감탄했다. 과거의 꿈과 현재의 현실 사이에 커다란 격차가 있었지만 그는 우리의 새 삶을 사랑할 준비가 되어 있었다. 나도 그 삶을 사랑하기 위해 노력해보겠다고 결심했다.
+          </p>
+          <p>- Lab Girl 中, Hope Jahren</p>
         </div>
         <h3 className="profileName">Name : 서현정 (Seo Hyunjeong)</h3>
         <div className="profileSkills">
